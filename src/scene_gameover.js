1d99ab8a6881bc3661891c2cb73ba49b849fec91
Shaun Williams <shaunewilliams@gmail.com>
2013-08-09 08:03:33 -0500

    added low res score digits to game over screen

--- src/scene_gameover.js
+++ src/scene_gameover.js
@@ -6,6 +6,8 @@ Ptero.scene_gameover = (function(){
 
 	var replayBtn,quitBtn;
 
+	var highScoreSprite, highScorePos;
+
 	function cleanup() {
 		replayBtn.disable();
 		quitBtn.disable();
@@ -16,9 +18,39 @@ Ptero.scene_gameover = (function(){
 		Ptero.fadeToScene(scene,0.5);
 	}
 
+	var scoreBoard,digits;
+	var scorePos;
+	function makeScoreBoard() {
+		var m = Ptero.assets.mosaics['timertype'];
+		
+		digits = Ptero.score.getTotal() + "";
+
+		// get the total width of the score
+		var i,len=digits.length;
+		var d;
+		var w=0,h;
+		for (i=0; i<len; i++) {
+			d = digits[i]
+			h = m.frames[d].origSize.height;
+			w += m.frames[d].origSize.width;
+		}
+
+		// create a billboard of that size of the desired scale
+		scoreBoard = new Ptero.Billboard(w/2,h/2,w,h,2);
+
+		scorePos = Ptero.screen.screenFracToSpace(0.5, 0.2 );
+	}
+
 	function init() {
 		Ptero.audio.getScoreSong().play();
 		Ptero.overlord.stopScript();
+
+		makeScoreBoard();
+
+		highScoreSprite = Ptero.assets.sprites['menu_highscore'];
+		highScorePos = Ptero.screen.screenFracToSpace(0.5, 0.3);
+
+		scorePos = Ptero.screen.screenFracToSpace(0.5, 0.2 );
 
 		var w = 600;
 		var h = 720;
@@ -77,8 +109,43 @@ Ptero.scene_gameover = (function(){
 		Ptero.orb.draw(ctx);
 		replayBtn.draw(ctx);
 		quitBtn.draw(ctx);
+
+		// draw score
+		ctx.save();
+		scoreBoard.transform(ctx, scorePos);
+		var m = Ptero.assets.mosaics['timertype'];
+		var j,len=digits.length;
+		var d;
+		var x=0,y=0;
+		for (j=0; j<len; j++) { // each digit
+			d = digits[j];
+
+			var frame = m.frames[d];
+			var size = frame.origSize;
+
+			var tiles = frame.tiles;
+			var i,numTiles = tiles.length;
+			var sx,sy,w,h,dx,dy;
+			var tile;
+			for (i=0; i<numTiles; i++) { // each segment of digit
+				tile = tiles[i];
+
+				sx = tile.x;
+				sy = tile.y;
+				w = tile.w;
+				h = tile.h;
+				dx = x+tile.origX;
+				dy = tile.origY;
+
+				ctx.drawImage(m.img, sx,sy,w,h,dx,dy,w,h);
+			}
+
+			x += size.width;
+		}
+		ctx.restore();
+
 		if (newHighScore) {
-			//newHighBtn.draw(ctx);
+			highScoreSprite.draw(ctx, highScorePos);
 		}
 	}
 
