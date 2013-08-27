57c1be5f8a6fa0c0d7966efe42ce26cda93cbe99
Shaun Williams <shaunewilliams@gmail.com>
2013-08-27 09:26:42 -0500

    pinboard: enable zooming and panning when alt key is pressed

--- src/pinboard/scene_pinboard.js
+++ src/pinboard/scene_pinboard.js
@@ -1,47 +1,66 @@
 
 Ptero.Pinboard.scene_pinboard = (function(){
 
+	var isZoomPanKey;
 	var touchHandler = (function(){
-		var initialWindowLeft = null;
-		var initialWindowRight = null;
 		var dx,dy;
+		var isZoomPan;
 		
 		return {
 			coord: "canvas",
 			start: function(cx,cy) {
-				var c = Ptero.screen.getWindowPos();
-				dx = cx - c.x;
-				dy = cy - c.y;
+				isZoomPan = isZoomPanKey;
+				if (isZoomPan) {
+					var c = Ptero.screen.getWindowPos();
+					dx = cx - c.x;
+					dy = cy - c.y;
+				}
 			},
 			move: function(cx,cy) {
-				Ptero.screen.setWindowPos(cx-dx, cy-dy);
+				if (isZoomPan) {
+					Ptero.screen.setWindowPos(cx-dx, cy-dy);
+				}
 			},
 			end: function(cx,cy) {
+				isZoomPan = false;
 			},
 			cancel: function(cx,cy) {
+				isZoomPan = false;
 			},
 			scroll: function(cx,cy,delta,deltaX,deltaY) {
-				// from: http://stackoverflow.com/questions/2916081/zoom-in-on-a-point-using-scale-and-translate
-				var scaleFactor = Math.pow(1 + Math.abs(deltaY)/4 , deltaY > 0 ? 1 : -1);
+				if (isZoomPanKey) {
+					// from: http://stackoverflow.com/questions/2916081/zoom-in-on-a-point-using-scale-and-translate
+					var scaleFactor = Math.pow(1 + Math.abs(deltaY)/4 , deltaY > 0 ? 1 : -1);
 
-				var scale = Ptero.screen.getWindowScale() * scaleFactor;
-				var maxScale = Ptero.screen.getWindowFitScale();
-				var minScale = maxScale / 8;
-				scale = Math.max(minScale, Math.min(maxScale, scale));
+					var scale = Ptero.screen.getWindowScale() * scaleFactor;
+					var maxScale = Ptero.screen.getWindowFitScale();
+					var minScale = maxScale / 8;
+					scale = Math.max(minScale, Math.min(maxScale, scale));
 
-				Ptero.screen.zoomWindow(scale, cx, cy);
+					Ptero.screen.zoomWindow(scale, cx, cy);
+				}
 			},
 		};
 	})();
 
 	function init() {
 		Ptero.setBackground('menu');
-
 		var s = Ptero.screen;
 		s.setWindowScale(s.getCanvasHeight() / (s.getWindowHeight()*1.5));
 		s.centerWindowAtPixel(s.getCanvasWidth()/2, s.getCanvasHeight()/2);
 
 		Ptero.input.addTouchHandler(touchHandler);
+		window.addEventListener("keydown", function(e) {
+			if (e.keyCode == 18) {
+				isZoomPanKey = true;
+			}
+		});
+		window.addEventListener("keyup", function(e) {
+			if (e.keyCode == 18) {
+				isZoomPanKey = false;
+			}
+		});
+
 	}
 	
 	function cleanup() {
