8df27aa1c69653176f2b47ac0ba4245d86a843dd
Shaun Williams <shaunewilliams@gmail.com>
2013-09-18 03:18:24 -0500

    added backplates and title to game over screen

--- src/scene_gameover.js
+++ src/scene_gameover.js
@@ -3,6 +3,9 @@ Ptero.scene_gameover = (function(){
 
 	var buttonList;
 
+	var backplateBtn;
+	var backplateVolcanoBtn;
+
 	var isNewHigh;
 
 	function cleanup() {
@@ -42,6 +45,11 @@ Ptero.scene_gameover = (function(){
 			Ptero.audio.play('theme');
 		};
 
+		backplateBtn = btns["backplate"];
+		backplateVolcanoBtn = btns["backplateVolcano"];
+		backplateBtn.shouldDraw = false;
+		backplateVolcanoBtn.shouldDraw = false;
+
 		// enable controls after one second to prevent inadvertent selection if swipe actions spill over from the game
 		setTimeout(function() {
 			btns["replay"].enable();
@@ -49,12 +57,32 @@ Ptero.scene_gameover = (function(){
 		}, 1000);
 
 		isNewHigh = Ptero.score.commitStats();
-		btns["highScore"].shouldDraw = isNewHigh.score;
+		if (!isNewHigh.score) {
+			btns["highScore"].text = "game over";
+		}
 	}
 
 	function draw(ctx) {
 		Ptero.deferredSprites.draw(ctx);
+		ctx.fillStyle = "rgba(0,0,0,0.5)";
+		ctx.fillRect(0,0,Ptero.screen.getWindowWidth(),Ptero.screen.getWindowHeight());
 		if (!isExiting) {
+
+			var stage = Ptero.background.name;
+			var btn;
+			if (stage == "ice") {
+				btn = backplateBtn;
+				btn.image = Ptero.assets.images["backplate_ice"];
+			}
+			else if (stage == "volcano") {
+				btn = backplateVolcanoBtn;
+			}
+			else {
+				btn = backplateBtn;
+				btn.image = Ptero.assets.images["backplate_mountain"];
+			}
+			btn.draw(ctx);
+
 			buttonList.draw(ctx);
 		}
 	}
